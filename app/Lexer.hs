109aaf9eb5fbd2ed89bf31f9ac08a2e9702d1f73
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-09-10 18:04:13 +0200

    allows more whitespace

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -7,14 +7,17 @@ data Token =
     | HtmlNode (String, [Token])
     deriving (Show)
 
-lexChar :: Char -> String -> Maybe (Char, String)
+isWhite :: Char -> Bool
+isWhite char = char == ' ' || char == '\n'
+
+lexChar :: String -> String -> Maybe (Char, String)
 lexChar target input = case input of
-    (c:cs) | c == target -> Just (target, cs)
+    (c:cs) | c `elem` target -> Just (c, cs)
     _ -> Nothing
 
 lexString :: String -> Maybe (Token, String)
 lexString target = 
-    lexChar '"' target 
+    lexChar "\"" target 
     >>= (\(c, text) -> return (toStringResult (break (== '"') text)))
 
 toStringResult :: (String, String) -> (Token, String)
@@ -22,25 +25,25 @@ toStringResult (parsed, next) = (HtmlString parsed, tail next)
 
 lexNode :: String -> Maybe (Token, String)
 lexNode target = 
-    lexChar '(' target
-    >>= (\(c, text) -> return (break (== ' ') text))
-    >>= (\(name, next_str) -> case lexArguments next_str of 
+    lexChar "(" target
+    >>= (\(c, text) -> return (break isWhite text))
+    >>= (\(name, next_str) -> case lexArguments (dropWhile isWhite next_str) of 
         (Just (arguments, next)) -> return (HtmlNode (name, arguments), next)
         Nothing -> Nothing
     )
 
 lexArguments :: String -> Maybe ([Token], String)
-lexArguments target  
-    | isJust (lexChar ')' target) = Just ([], tail target)
-    | otherwise =
-            lexChar ' ' target
-            >>= (
-                \(prev, next) -> 
-                    case lexString next of
-                        (Just string) -> return string
-                        Nothing -> lexNode next
+lexArguments target =
+        (
+            case lexString target of
+                (Just string) -> return string
+                Nothing -> lexNode target
+        )
+        >>= (\(argument, next_str) ->
+            let (_, next) = span isWhite next_str in
+            case lexChar ")" next of
+                (Just (_, string)) -> return ([argument], string)
+                Nothing -> case lexArguments next of
+                    Just (args, next_string) -> return (argument : args, next_string)
+                    Nothing -> Nothing 
             )
-        >>= (\(argument, next) -> 
-            let Just (args, next_string) = lexArguments next in 
-                return (argument : args, next_string))
-
--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,26 @@
 module Main where
 import Lexer (lexNode)
 import Parser (parse)
+import System.Environment (getArgs)
+import Data.String (String)
+import Data.Maybe (Maybe(Nothing, Just))
+import GHC.Base (IO)
+import Prelude
 
 transpile :: String -> String
 transpile lisp = case lexNode lisp >>= (\(token, next) -> parse token) of 
     (Just result) -> result
-    Nothing -> "Error"
+    Nothing-> "Error"
+
+parseArgs :: [String] -> IO () 
+parseArgs [target, result] = do
+    content <- readFile target
+    writeFile result (transpile content)
+
+parseArgs _ = putStrLn "Error"
 
 main :: IO ()
-main = print (transpile "(html (head (title \"cs\")) (body \"cs\"))")
+main = do
+    args <- getArgs
+    parseArgs args
 
